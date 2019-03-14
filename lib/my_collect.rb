65a4a593d8dd919e2d402eca48cc30ae0140e3b3
Ethan <ethansagin@gmail.com>
2019-03-14 01:12:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
+  new = []
   i = 0
   while i < collection.length
-    yield collection[i]
+    new.push(yield collection[i])
     i += 1
   end
-  collection
+  new
 end
 
